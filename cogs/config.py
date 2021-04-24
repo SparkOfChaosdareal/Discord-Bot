53deb3192fe33e40c3800fd94f6bd3bd428bcf79
Florentin <floerge@gmail.com>
2021-04-24 12:01:55 +0200

    cogs added

---
+++ cogs/config.py
@@ -0,0 +1,14 @@
+import discord
+from discord.ext import commands
+
+class Config(commands.Cog):
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    # EVENT
+    @commands.Cog.listener()
+    async def 
+    
+def setup(bot):
+    bot.add_cog(Config(bot))
