d377907d75975c7d4296c733a9146bb6547c0a29
Florentin <floerge@gmail.com>
2021-05-07 09:52:28 +0200

    backup on 7.5.21

--- DiscordBot.py
+++ DiscordBot.py
@@ -8,6 +8,8 @@ import os
 
 from itertools import cycle
 
+from typing_extensions import TypeAlias
+
 cfg = configparser.ConfigParser()
 cfg.sections()
 
@@ -25,63 +27,64 @@ async def on_ready():
     
 
 # BOT EVENT CATJAM
-@bot.event
+@bot.listen()
 async def on_message(message):
     cfg.read('config.cfg')
     # RETURN IF MESSAGE WAS SEND BY BOT
     if message.author == bot.user:
         return
     # PUT A CATJAM GIF IN CHAT IF CATJAM IST SET TRUE IN CFG AND MESSAGE CONTAINS WORD FROM CATJAM.JSON
-    if(cfg['GIFR']['catjam'] == 'true'):
-        with open('catjam.json', 'r') as catjam:
+    if cfg['GIFR']['catjam'] == "true":
+        with open('catjam.json', 'r', encoding="utf8") as catjam:
             data = json.load(catjam)
         for word in data:
             if word in message.content:
                 await message.channel.send('a') 
                 break
         catjam.close()
-    #if 'ping' or 'Ping' in message.content:
-        #await message.channel.send("Pong!")
-"""
+    if 'ping' in message.content or 'Ping' in message.content:
+        await message.channel.send("Pong!")
+
 # PRINTS TO COLSOLE ON MEMBER REMOVE
 @bot.event
 async def on_member_remove(member):
     print(f'{member} has left a server')
 
 # SIMPLE PING COMMAND
-@bot.command
+@bot.command()
 async def ping(ctx):
-    await ctx.sent(f'Pong! {round(bot.latency * 1000)}ms')
+    await ctx.send(f'Pong! {round(bot.latency * 1000)}ms')
 
 # CLEARS AMOUNT NUMBER OF MESSAGES DEFAULT VALUE = 5
-@bot.command
+@bot.command()
 @commands.has_permissions(manage_messages=True)
 async def clear(ctx, amount=5):
     await ctx.channel.purge(limit=amount)
 
 # SIMPLE PRINT COMMAND
-@bot.command(help="prints out the message you told him to print", brief="prints out ur stupid shit")
-async def print(ctx, *args):
+@bot.command()
+async def pprint(ctx, *, args):
     for arg in args:
+        print (arg)
         await ctx.send(arg)
-"""
+
 @tasks.loop(seconds=10)
 async def change_status():
     await bot.change_presence(activity=discord.Streaming(name=next(status), url='https://twitch.tv/spark0fchaos'))
  
-@bot.command
+@bot.command()
 @commands.check(ist_it_me)
 async def load(ctx, extension):
     bot.load_extension(f'cogs.{extension}')
     await ctx.send(f'successfully loaded {extension}')
 
-@bot.command
+@bot.command()
 @commands.check(ist_it_me)
 async def unload(ctx, extension):
     bot.unload_extension(f'cogs.{extension}')
     await ctx.send(f'successfully unloaded {extension}')
 
-@bot.command
+@bot.command()
 @commands.check(ist_it_me)
 async def reload(ctx, extension):
     bot.unload_extension(f'cogs.{extension}')
@@ -93,4 +96,4 @@ for filename in os.listdir('./cogs'):
         bot.load_extension(f'cogs.{filename[:-3]}')
 
 # RUNS THE BOT
-bot.run("ODAxNzczNDUwNDY0OTg1MTI4.YAljtg.ykgA-Mv3K2XvQqI8xskGYqR8Njg")
+bot.run("ODM1NDk1ODg2NzY2NzM1Mzcw.YIQSLg.S20ieFv3RFN_TfP3lXS4t_ar5VE")
